6e47387888172b6c57686fa9e2a5e78b26db0cd9
Brian <46753834+Shankk@users.noreply.github.com>
2020-09-25 14:21:26 -0400

    Added Basic UI

---
+++ Assets/Scripts/BtnEventBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BtnEventBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartBtnBehavior()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void TutorialBtnBehavior()
+    {
+        SceneManager.LoadScene("TutorialScene");
+    }
+
+    public void ReturnBtnBehavior()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+
+    public void ExitBtnBehavior()
+    {
+        Application.Quit();
+    }
+}
