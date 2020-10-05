2509b59acbe33f3ef86c9180e70af5643fc72ef4
Brian <46753834+Shankk@users.noreply.github.com>
2020-10-04 20:56:39 -0400

    Disregard This

---
+++ Assets/Printcode.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Printcode : MonoBehaviour
+{
+
+    public delegate int Del(int arg1);
+    public event Del evt;
+    public int Callback(int arg)
+    {
+        Debug.Log("Hello");
+        return 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        evt = Callback;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+
+        {
+
+            evt.Invoke(5);
+
+        }
+    }
+
+}
